1ca6a843795545bd946da03e9523ce632055c11d
Samuel Souza <ssouza.himself@gmail.com>
2023-12-23 10:07:20 -0300

    debug step by step

--- chapter5/grita.sh
+++ chapter5/grita.sh
@@ -5,9 +5,19 @@
 # Mostra a palavra ($TXT) em maiúscula e com exclamações
 # Exemplo: foo -> !!!!!FOO!!!!!
 
+DEBUG=1					# Debug: 0 desliga, 1 liga
+
+Debug(){
+  [ "$DEBUG" = 1 ] && echo "$*"
+}
+
 TXT="gritaria"
 
 TXT="     $TXT     "			# Adiciona 5 espaços ao redor
-TXT=$(echo "$TXT" | tr ' ' '!')           # Troca os espaços or exclamações
+
+Debug "TXT com espaços    : [$TXT]"
+TXT=$(echo "$TXT" | tr ' ' '!')         # Troca os espaços or exclamações
+Debug "TXT com exclamações: [$TXT]"
+
 TXT=$(echo "$TXT" | tr a-z A-Z)		# Deixa o texto em maiúsculas
 echo $TXT				# Mostra a mensagem
